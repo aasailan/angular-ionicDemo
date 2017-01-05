ce36b6f10dd5535ce06c12a9cdcd1813411a98df
aasailan <184443035@qq.com>
2017-01-05 21:14:01 +0800

    add tab-angular view and tab-ionic view

--- www/js/app.js
+++ www/js/app.js
@@ -37,49 +37,25 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     abstract: true,
     templateUrl: 'templates/tabs.html'
   })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
+  .state('tab.ionic', {
+    url: '/ionic',
     views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
+      'tab-ionic': {
+        templateUrl: 'templates/ionic/tab-ionic.html',
+        // controller: 'ionicTabCtrl'
       }
     }
   })
-
-  .state('tab.chats', {
-      url: '/chats',
+  .state('tab.angular', {
+      url: '/angular',
       views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
+        'tab-angular': {
+          templateUrl: 'templates/angular/tab-angular.html',
+          // controller: 'ChatsCtrl'
         }
       }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
+    });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
-
+  $urlRouterProvider.otherwise('/tab/ionic');
 });
